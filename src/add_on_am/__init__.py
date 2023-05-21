9ee27579b2700e04a7b27f285f03cc39d4c30b75
Tim <tim.daffner@tum.de>
2023-05-21 19:08:02 +0200

    honning in onto ideal position added to sectioning

--- src/add_on_am/__init__.py
+++ src/add_on_am/__init__.py
@@ -21,8 +21,8 @@ Describe typical classes found in project
 """
 
 from .sample_module import SampleClassName
-from .wall import Wall, Map2d
+from .wall import Wall, Map2d, Map2d_optimized
 from .reachability_map import ReachabilityMap, ReachabilityMap2D, Envelope
 from path_planner import SurfacePathPlanner
 
-__all__ = ['SampleClassName', "Wall", "ReachabilityMap", "ReachabilityMap2D", "Envelope", "SurfacePathPlanner", "Map2d"]
+__all__ = ['SampleClassName', "Wall", "ReachabilityMap", "ReachabilityMap2D", "Envelope", "SurfacePathPlanner", "Map2d", "Map2d_optimized"]
--- src/add_on_am/wall.py
+++ src/add_on_am/wall.py
@@ -137,6 +137,7 @@ class Map2d:
         self.resolution = resolution
         self.x_size = int(wall.width / resolution) + 1
         self.wall = wall
+        self.length = 0
 
     def create_network(self, envelope):
         reach = int((envelope.r_out - envelope.r_in) / self.resolution) + 1
@@ -161,7 +162,7 @@ class Map2d:
                     }
                 self.network.add_node(key=index, attr_dict=attr_dict)
                 index += 1
-            
+        self.length = index
         return self.network
     
     def assign_reach(self, envelope):
@@ -223,3 +224,151 @@ class Map2d:
 
         return positions, others
 
+
+class Map2d_optimized(Map2d):
+    # adding a finder grid of positions to the given network of positions at a given position with a given resolution (dist/3)    
+    def add_to_network(self, pos, envelope, dist=None):
+        [pos_x, pos_y] = self.network.node_attributes(pos, ["x", "y"])
+        number_p = 9
+        size = int(math.sqrt(number_p))
+        if dist == None:
+            dist = self.resolution
+        new_res = dist/size
+        print("resolution: ", new_res, size)
+        index = self.length + 1
+        for i in range(size):
+            x_l = pos_x + i* new_res
+            x_r = pos_x - i* new_res
+            sin_l = -self.wall.sin_wave(self.wall.down_amp, self.wall.down_freq, self.wall.down_phase, x_l)
+            sin_r = -self.wall.sin_wave(self.wall.down_amp, self.wall.down_freq, self.wall.down_phase, x_r)
+            for j in range(size):
+                y_l = pos_y + j* new_res
+                y_r = pos_y - j* new_res
+                # not pos
+                if (i!=0 or j!=0) and envelope.r_in < abs(sin_l-y_l) < envelope.r_out :
+                    attr_dict = {
+                        'x':x_l, 'y':y_l, 'z':0,
+                        "ri": 0,
+                        "vertices": [],
+                        }
+                    self.network.add_node(key=index, attr_dict=attr_dict)
+                    index += 1
+                # different x than pos
+                if i!=0 and envelope.r_in < abs(sin_r-y_l) < envelope.r_out:
+                    attr_dict = {
+                        'x':x_r, 'y':y_l, 'z':0,
+                        "ri": 0,
+                        "vertices": [],
+                        }
+                    self.network.add_node(key=index, attr_dict=attr_dict)
+                    index += 1
+                # different j than pos
+                if j!=0 and envelope.r_in < abs(sin_l-y_r) < envelope.r_out:
+                    attr_dict = {
+                        'x':x_l, 'y':y_r, 'z':0,
+                        "ri": 0,
+                        "vertices": [],
+                        }
+                    self.network.add_node(key=index, attr_dict=attr_dict)
+                    index += 1
+                # different y and x than pos
+                if i!=0 and j!=0 and envelope.r_in < abs(sin_r-y_r) < envelope.r_out:
+                    attr_dict = {
+                        'x':x_r, 'y':y_r, 'z':0,
+                        "ri": 0,
+                        "vertices": [],
+                        }
+                    self.network.add_node(key=index, attr_dict=attr_dict)
+                    index += 1
+        self.length = index
+    
+
+    # finding the the ideal position out of an envelope of positions from all positions and from a given starting node
+    def pos_by_path_limited(self, planner, envelope, current_pos, start_node, dist=None):
+        start_pos = self.length + 1
+        self.add_to_network(current_pos, envelope, dist)
+        positions = self.network.nodes()
+        reachable_points = []
+        # run is used to skip until the start node for this section of the path
+        run = False
+        for node in planner.network.path:
+            if node == start_node:
+                run = True
+            if not run:
+                continue
+            reachable_pos = []
+            # looping over the positions from which the previous nodes were reachable and appending them to the next iteration if current node is also reachable
+            for pos in positions:
+                if pos >= start_pos:
+                    [envelope.x, envelope.y] = self.network.node_attributes(pos, ["x", "y"])
+                    [x, y, z] = planner.network.node_attributes(node, ["x", "y", "z"])
+                    if envelope.point_inside(x, y, z):
+                        reachable_pos.append(pos)
+            if len(reachable_pos) == 0:
+                # if there is no more position from which the current node can be reached, add the previous position to the dict and start new with current node
+                return positions[0], reachable_points
+            else:
+                positions = reachable_pos
+                reachable_points.append(node)
+        return positions[-1], reachable_points
+    
+
+    # optimizing the position from a given position by adding a finder grid of positions around the given position
+    def optimize_pos(self, planner, envelope, ideal_pos, reachable_points, loops=2):
+        for i in range(loops):
+            if i > 0:
+                res = self.resolution/(i*3)
+            else:
+                res = self.resolution
+            # first optimization step (res = self.resolution / 3 = 0.1333)
+            # second optimization step (res = self.resolution / 9 = 0.0444)
+            improved_pos, improved_points = self.pos_by_path_limited(planner, envelope, ideal_pos, reachable_points[0], res)
+            if len(improved_points) > len(reachable_points):
+                ideal_pos = improved_pos
+                reachable_points = improved_points
+        return ideal_pos, reachable_points
+    
+
+    # finding the ideal positions to print as much as possible of the continous path
+    def pos_by_path(self, planner, envelope):
+        reach = {}
+        positions = self.network.nodes()
+        reachable_points = []
+        # run is used to skip the nodes after the optimization
+        run = True
+        last_node = None
+        for node in planner.network.path:
+            if node == last_node:
+                run = True
+                continue
+            if not run:
+                continue
+            reachable_pos = []
+            # looping over the positions from which the previous nodes were reachable and appending them to the next iteration if current node is also reachable
+            for pos in positions:
+                [envelope.x, envelope.y] = self.network.node_attributes(pos, ["x", "y"])
+                [x, y, z] = planner.network.node_attributes(node, ["x", "y", "z"])
+                if envelope.point_inside(x, y, z):
+                    reachable_pos.append(pos)
+            if len(reachable_pos) == 0:
+                ideal_pos, reachable_points = self.optimize_pos(planner, envelope, positions[0], reachable_points)
+                # if there is no more position from which the current node can be reached, add the previous position to the dict and start new with current node
+                reach[ideal_pos] = reachable_points
+                print(reachable_points[-1], node)
+                if node != reachable_points[-1]:
+                    last_node = reachable_points[-1]
+                    run = False
+                positions = self.network.nodes()
+                reachable_points = []
+            else:
+                positions = reachable_pos
+                reachable_points.append(node)
+        
+        # for some reason dicts cant be ordered as wanted and are always ordered by length of items, no idea who had this ingenius idea
+        # last value of list is choosen for last position, as least overlap with previous position???
+
+        # optimization for last one not necessary as there is a whole envelope of points that works
+        # ideal_pos, reachable_points = self.optimize_pos(planner, envelope, positions[-1], reachable_points)
+        reach[positions[-1]] = reachable_points
+
+        return reach, positions
